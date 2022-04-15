cc289531c8f5e79587edf5a542823e1e6145ac5a
McClendonJustin <mcclendon.justin.a@gmail.com>
2022-04-15 17:45:17 -0500

    initialize

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Board from "./components/Board";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Board />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
